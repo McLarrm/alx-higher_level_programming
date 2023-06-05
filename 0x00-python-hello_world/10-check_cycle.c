912cdbf977e6ba567517a12f5fa3b63e9e346e30
Olajide <larrm_ola@live.com>
2023-06-05 16:07:56 -0700

    Function that checks if a singly linked has a cycle in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,46 +13,17 @@ int check_cycle(listint_t *list)
 		return (0);
 
 	slow = list;
-	fast = list->next;
+	fast = list;
 
 	while (fast != NULL && fast->next != NULL)
 	{
-		if (slow == fast)
-			return (1);
-
 		slow = slow->next;
 		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
 	}
 
 	return (0);
 }
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int hasCycle;
-
-	/* Create a linked list with a cycle */
-	listint_t *list = malloc(sizeof(listint_t));
-	list->data = 1;
-	list->next = malloc(sizeof(listint_t));
-	list->next->data = 2;
-	list->next->next = malloc(sizeof(listint_t));
-	list->next->next->data = 3;
-	list->next->next->next = list;
-
-	/* Check if the linked list has a cycle */
-	hasCycle = check_cycle(list);
-	printf("Has Cycle: %d\n", hasCycle);
-
-	/* Free the allocated memory */
-	free(list->next->next);
-	free(list->next);
-	free(list);
-
-	return (0);
-}

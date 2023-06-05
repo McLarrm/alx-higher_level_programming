1033edf41dbf66ffa2574335bfeb829fb11d8748
Olajide <larrm_ola@live.com>
2023-06-05 16:13:15 -0700

    Header File

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,12 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Definition of a singly linked list node */
 typedef struct listint_s {
-    int data;
+    int n;
     struct listint_s *next;
 } listint_t;
 
 /* Prototype */
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */

d5423ac32f358f6eb2f59e35a45c6d608c9ce69a
Olajide <larrm_ola@live.com>
2023-06-07 02:37:42 +0000

    Function that checks for uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        uppercase_char = chr(ord(char) - 32)
+        if ord('a') <= ord(char) <= ord('Z'):
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
         print("{}".format(uppercase_char), end="")
     print()

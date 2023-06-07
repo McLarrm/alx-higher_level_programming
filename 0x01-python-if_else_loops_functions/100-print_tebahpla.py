de63448c143918b609ad8f744ac09ac8927af2d3
Olajide <larrm_ola@live.com>
2023-06-07 02:17:38 +0000

    Program that prints ASCII alphabets in reverse prder

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{}{}".format(chr(i + 32), chr(i)), end="")
+for i in range(122, 96, -1):
+    if (122 - i) % 2 == 0:
+        print(chr(i), end="")
+    else:
+        print(chr(i - 32), end="")

130bee4cb83a183934323f5353d9c2e5da102bc8
Olajide <larrm_ola@live.com>
2023-06-09 03:44:56 -0700

    Program that prints and list the number of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     argv = sys.argv[1:]
 
-    print("{} argument{}{}"
-            .format(argc, 's'if argc != 1 else '', ':'if argc else '.'))
+    print("{} argument{}{}".format(argc, 's' if argc != 1 else '', ':' if argc else '.'))
+
     for i, arg in enumerate(argv, start=1):
         print("{}: {}".format(i, arg))
+

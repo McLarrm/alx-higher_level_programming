52d1cda8f471dc14cf6bded2a9a7edd156227e7f
McLarrm <larrm_ola@live.com>
2023-07-31 10:13:38 -0700

    Class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """ Returns the string representation of the rectangle """
-        return "[Rectangle] ({}) {}/{} - {}/{}"
-    .format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, 
+                self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """ Updates the attributes of the rectangle based on the arguments """

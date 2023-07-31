e7f5eec18387b2d37a8605950b4ac834e92521b7
McLarrm <larrm_ola@live.com>
2023-07-31 11:38:17 -0700

    Class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,3 +42,10 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except FileNotFoundError:
             return []
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON representation of a list of dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)

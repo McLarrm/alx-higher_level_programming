bc5dabe5abcd41f7a5a5bb0aa37fc12b4c64d4b6
McLarrm <larrm_ola@live.com>
2023-07-31 08:05:33 -0700

    Function that adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,10 +2,10 @@
 """ Function that adds two integer values"""
 
 
-def add_integer(a: int, b: int = 98) -> int:
-    """Adds two numbers as integers, with a default value of 98 for the second number"""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or a float")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or a float")
+def add_integer(a, b=98):
+    """ Adds two numbers as integers """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
     return int(a) + int(b)

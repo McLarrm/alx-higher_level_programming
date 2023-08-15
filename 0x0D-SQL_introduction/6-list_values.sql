9f5f7bf3a0ebc93b974950da759a6eb62f4a607d
McLarrm <larrm_ola@live.com>
2023-08-15 09:07:33 -0700

    Script that lists all rows of the table first_table from the database

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,5 +1,3 @@
 -- Script: List All Rows of first_table
 -- Task: List all rows of the table first_table from the database hbtn_0c_0
-SET @database_name = IFNULL(@@ARGS[1], 'hbtn_0c_0');
-SELECT *
-FROM @database_name.first_table;
+SELECT * FROM first_table;

950be12d14177502e8584a918f381285d3850f87
McLarrm <larrm_ola@live.com>
2023-08-15 09:31:21 -0700

    Script that removes all records with a score <= 5 in the table second_table of the database

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script: Remove Records with score <= 5 in second_table
+-- Task: Remove all records with a score <= 5 in the table second_table of the database hbtn_0c_0
+DELETE FROM second_table WHERE score <= 5;

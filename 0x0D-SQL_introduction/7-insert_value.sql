6be3273661a8019096fec6b16fa3415e9fe9c8de
McLarrm <larrm_ola@live.com>
2023-08-15 09:11:06 -0700

    Script that inserts a new row in the table first_table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script: Insert New Row into first_table
+-- Task: Insert a new row into the table first_table in the database hbtn_0c_0
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');

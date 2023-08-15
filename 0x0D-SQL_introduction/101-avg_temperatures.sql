2a873c14bb0858cf8cfc60ccf257d6f62e4a303e
McLarrm <larrm_ola@live.com>
2023-08-15 12:28:10 -0700

    Script that displays the average temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Script: Display Average Temperature by City
+-- Task: Display the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC

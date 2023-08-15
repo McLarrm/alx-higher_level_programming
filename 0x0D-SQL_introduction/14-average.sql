a48206299d472f61a4c683cb67cbb7ff574cd855
McLarrm <larrm_ola@live.com>
2023-08-15 09:33:32 -0700

    Script that computes the score average of all records in the table second_table of the database

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script: Compute Average Score in second_table
+-- Task: Compute the average score of all records in the table second_table of the database hbtn_0c_0
+SELECT AVG(score) AS average FROM second_table;

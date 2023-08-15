5135b65b96d9b845aac9f6936e7315a7bb768927
McLarrm <larrm_ola@live.com>
2023-08-15 09:36:09 -0700

    Script that lists the number of records with the same score in the table second_table of the database

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- Script: List Number of Records with Same Score in second_table
+-- Task: List the number of records with the same score in the table second_table of the database hbtn_0c_0
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;

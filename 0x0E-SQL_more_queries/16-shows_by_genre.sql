8f00c204940c12bb5347f3177d04e54f680cc687
McLarrm <larrm_ola@live.com>
2023-08-16 14:42:35 -0700

    Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Script: List Shows and Linked Genres (Including NULL)
+-- Task: List all shows along with linked genres from the hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name

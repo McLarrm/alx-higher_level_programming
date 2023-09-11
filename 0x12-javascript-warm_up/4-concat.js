57abbb9653cab7cbd2e800f33c87edabc4015405
McLarrm <larrm_ola@live.com>
2023-09-11 15:22:21 -0700

    Script that prints two arguments passed to it in a certain format

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log('Please provide two arguments.');
-} else {
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);

2774d8f384599bdb552e690b433c829d14e5f174
McLarrm <larrm_ola@live.com>
2023-09-17 10:43:03 -0700

    Script that lists all State object from the database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Prints the first State object from the database """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password

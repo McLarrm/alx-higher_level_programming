fd931c06adcb4c5911cf0aac4b2468e61ae2314d
McLarrm <larrm_ola@live.com>
2023-09-17 11:08:50 -0700

    Script that changes the name of a State object with a name containing the letter "a" from the databate

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Deletes all State objects with a name containing the letter 'a'"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_to_delete:
+        session.delete(state)
+
+    session.commit()
+
+    session.close()

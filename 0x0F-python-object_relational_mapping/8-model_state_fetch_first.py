4bafbd694701b019e7e31e69a9c8d5be12cba74d
McLarrm <larrm_ola@live.com>
2023-09-17 10:54:23 -0700

    Script that lists all State object from the database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Prints the first State object from the database """
+"""Prints the first State object from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
@@ -8,4 +8,24 @@ from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    session.close()

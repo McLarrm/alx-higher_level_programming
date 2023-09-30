6e14d3ad2b67c16da57304669bb03392be3a3e13
McLarrm <larrm_ola@live.com>
2023-09-29 17:11:01 -0700

    Lists 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Lists 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails” """
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'),
+                                  com.get('commit').get('author').get('name')))
